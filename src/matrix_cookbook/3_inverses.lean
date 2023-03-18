821eb407b36098dcc1c23b7a357f8ae181c2ae5e
Eric Wieser <wieser.eric@gmail.com>
2023-03-18 14:43:35 +0000

    blank file for inverses

---
+++ src/matrix_cookbook/3_inverses.lean
@@ -0,0 +1,150 @@
+namespace matrix_cookbook
+
+/-! # Inverses -/
+
+/-! ## Basic -/
+
+/-! ### Definition -/
+
+lemma eq_145 : sorry := sorry
+
+/-! ### Cofactors and Adjoint -/
+
+lemma eq_146 : sorry := sorry
+lemma eq_147 : sorry := sorry
+lemma eq_148 : sorry := sorry
+
+/-! ### Determinant -/
+
+lemma eq_149 : sorry := sorry
+lemma eq_150 : sorry := sorry
+
+/-! ### Construction -/
+
+lemma eq_151 : sorry := sorry
+
+/-! ### Condition number -/
+
+lemma eq_152 : sorry := sorry
+lemma eq_153 : sorry := sorry
+lemma eq_154 : sorry := sorry
+
+/-! ## Exact Relations -/
+
+/-! ### Basic -/
+
+lemma eq_155 : sorry := sorry
+
+/-! ### The Woodbury identity -/
+
+lemma eq_156 : sorry := sorry
+lemma eq_157 : sorry := sorry
+lemma eq_158 : sorry := sorry
+
+/-! ### The Kailath Variant -/
+
+lemma eq_159 : sorry := sorry
+
+/-! ### Sherman-Morrison -/
+
+lemma eq_160 : sorry := sorry
+
+/-! ### The Searle Set of Identities -/
+
+lemma eq_161 : sorry := sorry
+lemma eq_162 : sorry := sorry
+lemma eq_163 : sorry := sorry
+lemma eq_164 : sorry := sorry
+lemma eq_165 : sorry := sorry
+lemma eq_166 : sorry := sorry
+lemma eq_167 : sorry := sorry
+
+/-! ### Rank-1 update of inverse of inner product -/
+
+/-! ### Rank-1 update of Moore-Penrose Inverse -/
+
+lemma eq_168 : sorry := sorry
+lemma eq_169 : sorry := sorry
+lemma eq_170 : sorry := sorry
+lemma eq_171 : sorry := sorry
+lemma eq_172 : sorry := sorry
+lemma eq_173 : sorry := sorry
+lemma eq_174 : sorry := sorry
+lemma eq_175 : sorry := sorry
+lemma eq_176 : sorry := sorry
+lemma eq_177 : sorry := sorry
+lemma eq_178 : sorry := sorry
+lemma eq_179 : sorry := sorry
+lemma eq_180 : sorry := sorry
+lemma eq_181 : sorry := sorry
+lemma eq_182 : sorry := sorry
+lemma eq_183 : sorry := sorry
+
+/-! ## Implication on Inverses -/
+
+lemma eq_184 : sorry := sorry
+
+/-! ### A PosDef identity -/
+
+lemma eq_185 : sorry := sorry
+
+/-! ## Approximations -/
+
+lemma eq_186 : sorry := sorry
+lemma eq_187 : sorry := sorry
+lemma eq_188 : sorry := sorry
+lemma eq_189 : sorry := sorry
+lemma eq_190 : sorry := sorry
+lemma eq_191 : sorry := sorry
+lemma eq_192 : sorry := sorry
+lemma eq_193 : sorry := sorry
+lemma eq_194 : sorry := sorry
+lemma eq_195 : sorry := sorry
+lemma eq_196 : sorry := sorry
+lemma eq_197 : sorry := sorry
+
+/-! ## Generalized Inverse -/
+
+lemma eq_198 : sorry := sorry
+
+/-! ### Definition -/
+
+/-! ## Pseudo Inverse -/
+
+lemma eq_199 : sorry := sorry
+lemma eq_200 : sorry := sorry
+lemma eq_201 : sorry := sorry
+lemma eq_202 : sorry := sorry
+lemma eq_203 : sorry := sorry
+lemma eq_204 : sorry := sorry
+lemma eq_205 : sorry := sorry
+lemma eq_206 : sorry := sorry
+lemma eq_207 : sorry := sorry
+lemma eq_208 : sorry := sorry
+lemma eq_209 : sorry := sorry
+lemma eq_210 : sorry := sorry
+lemma eq_211 : sorry := sorry
+lemma eq_212 : sorry := sorry
+lemma eq_213 : sorry := sorry
+lemma eq_214 : sorry := sorry
+lemma eq_215 : sorry := sorry
+lemma eq_216 : sorry := sorry
+
+/-! ### Definition -/
+
+lemma eq_217 : sorry := sorry
+lemma eq_218 : sorry := sorry
+lemma eq_219 : sorry := sorry
+lemma eq_220 : sorry := sorry
+
+/-! ### Properties -/
+
+lemma eq_221 : sorry := sorry
+lemma eq_222 : sorry := sorry
+
+/-! ### Construction -/
+
+lemma eq_223 : sorry := sorry
+lemma eq_224 : sorry := sorry
+
+end matrix_cookbook
