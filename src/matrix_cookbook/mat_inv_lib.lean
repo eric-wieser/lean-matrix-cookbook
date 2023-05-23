4b73d957160c2307c877ed93da458209ae541a7b
MohanadAhmed <m.a.m.elhassan@gmail.com>
2023-05-23 11:22:33 +0300

    Not very fruitful attempts at golfing

--- src/matrix_cookbook/mat_inv_lib.lean
+++ src/matrix_cookbook/mat_inv_lib.lean
@@ -51,13 +51,11 @@ end
 lemma row_mul_mat_mul_col (A : matrix m m ℂ) (b c : m → ℂ) :
   c ⬝ᵥ A.mul_vec b = (row c ⬝ A ⬝ col b) () ():= 
 begin
-  rw mul_apply,
-  rw dot_product,
+  rw [mul_apply, dot_product], 
   conv_rhs 
   { apply_congr, 
     skip, 
-    rw col_apply, 
-    rw mul_apply, 
+    rw [col_apply, mul_apply], 
     conv 
     { congr, 
       apply_congr, 
@@ -70,8 +68,7 @@ begin
     skip, 
     rw mul_vec, 
     dsimp, 
-    rw dot_product,
-    rw finset.mul_sum, 
+    rw [dot_product, finset.mul_sum], 
     conv 
     { apply_congr, 
       skip, 
@@ -84,33 +81,30 @@ lemma one_add_row_mul_mat_col_inv (A : matrix m m ℂ)
   (1 + c ⬝ᵥ A.mul_vec b)⁻¹ =
     (1 + row c ⬝ A ⬝ col b)⁻¹ punit.star punit.star :=
 begin
-  -- set β := (1 + row c ⬝ A⁻¹ ⬝ col b)⁻¹ punit.star punit.star,
   set γ := 1 + c ⬝ᵥ A.mul_vec b, 
-  rw ← mul_left_inj' habc, rw inv_mul_cancel habc,
-  rw mul_comm, rw inv_def, rw adjugate,
+  rw [← mul_left_inj' habc, inv_mul_cancel habc, mul_comm, inv_def, adjugate],
   simp only [
     det_unique, punit.default_eq_star, pi.add_apply, 
     one_apply_eq, ring.inverse_eq_inv', of_apply, pi.smul_apply,
     cramer_subsingleton_apply, pi.single_eq_same, 
     algebra.id.smul_eq_mul, mul_one
   ],
-  rw ← row_mul_mat_mul_col, symmetry', apply mul_inv_cancel, exact habc,
+  rw ← row_mul_mat_mul_col, 
+  symmetry', 
+  exact mul_inv_cancel habc, 
 end
 
 lemma sherman_morrison (A : matrix m m ℂ) (B : matrix n n ℂ) (U : matrix m n ℂ) (V : matrix n m ℂ) 
   {hA: is_unit A.det} {hB: is_unit B.det} {hQ: is_unit (B⁻¹ + V ⬝ A⁻¹ ⬝ U).det}:
   (A + U⬝B⬝V)⁻¹ = A⁻¹ - A⁻¹⬝U⬝(B⁻¹+V⬝A⁻¹⬝U)⁻¹⬝V ⬝ A⁻¹ := 
 begin
-  apply inv_eq_right_inv,
-  rw matrix.add_mul,
-  rw matrix.mul_sub, rw mul_nonsing_inv,
+  rw inv_eq_right_inv,
+  rw [matrix.add_mul, matrix.mul_sub, mul_nonsing_inv],
   repeat {rw matrix.mul_assoc A⁻¹ _ _},
-  rw mul_nonsing_inv_cancel_left,
   set Q := (B⁻¹+V⬝A⁻¹⬝U),
-  rw matrix.mul_sub,
-  rw sub_add_sub_comm,
-  rw matrix.mul_assoc _ Q⁻¹ _,  
-  rw matrix.mul_assoc U (Q⁻¹⬝V) _,
+  rw [mul_nonsing_inv_cancel_left, matrix.mul_sub, sub_add_sub_comm, 
+    matrix.mul_assoc _ Q⁻¹ _,  matrix.mul_assoc U (Q⁻¹⬝V) _],
+
   have : U ⬝ B ⬝ V ⬝ (A⁻¹ ⬝ (U ⬝ (Q⁻¹ ⬝ V ⬝ A⁻¹))) 
     = (U ⬝ B ⬝ V ⬝ A⁻¹ ⬝ U) ⬝ (Q⁻¹ ⬝ V ⬝ A⁻¹), 
   { rw ← matrix.mul_assoc _ A⁻¹ _,
@@ -118,12 +112,10 @@ begin
   rw this,
   rw ← matrix.add_mul,
   nth_rewrite 1 ← matrix.mul_one U,
-  rw ←  mul_nonsing_inv B, rw ← matrix.mul_assoc _ B _,
+  rw [←  mul_nonsing_inv B, ← matrix.mul_assoc _ B _],
   repeat {rw matrix.mul_assoc (U⬝B) _ _},
-  rw ← matrix.mul_add (U⬝B) _ _,
-  rw matrix.mul_assoc (U⬝B),
-  rw matrix.mul_assoc Q⁻¹,
-  rw mul_nonsing_inv_cancel_left, simp only [add_sub_cancel],
+  rw [← matrix.mul_add (U⬝B) _ _, matrix.mul_assoc (U⬝B), matrix.mul_assoc Q⁻¹,
+    mul_nonsing_inv_cancel_left, add_sub_cancel], 
   assumption',
 end
 
