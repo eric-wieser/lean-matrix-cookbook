afc35792a64eec660214e8d812d0905ecb20ef63
Eric Wieser <wieser.eric@gmail.com>
2023-05-26 16:14:01 +0000

    and the other lemma

--- src/matrix_cookbook/for_mathlib/linear_algebra/matrix/adjugate.lean
+++ src/matrix_cookbook/for_mathlib/linear_algebra/matrix/adjugate.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2023 Mohanad Ahmed. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Mohanad Ahmed
+Authors: Mohanad Ahmed, Eric Wieser
 -/
 
 import data.matrix.basic
@@ -20,7 +20,7 @@ open_locale matrix big_operators
 variables {R : Type*} [comm_ring R]
 
 lemma adjugate_eq_det_submatrix {n : ℕ} (A : matrix (fin n.succ) (fin n.succ) R) (i j) :
-  adjugate A i j = (-1)^(j + i : ℕ) * det (A.submatrix j.succ_above i.succ_above) := 
+  adjugate A i j = (-1)^(j + i : ℕ) * det (A.submatrix j.succ_above i.succ_above) :=
 begin
   simp_rw [adjugate_apply, det_succ_row _ j, update_row_self, submatrix_update_row_succ_above],
   rw [fintype.sum_eq_single i (λ h hjk, _), pi.single_eq_same, mul_one],
@@ -29,7 +29,7 @@ end
 
 lemma det_eq_sum_mul_adjugate_row {n : Type*} [decidable_eq n] [fintype n]
   (A : matrix n n R) (i : n) :
-  det A = ∑ j : n, A i j * adjugate A j i := 
+  det A = ∑ j : n, A i j * adjugate A j i :=
 begin
   haveI : nonempty n := ⟨i⟩,
   obtain ⟨n', hn'⟩ := nat.exists_eq_succ_of_ne_zero (fintype.card_ne_zero : fintype.card n ≠ 0),
@@ -42,3 +42,8 @@ begin
   rw det_succ_row A' (e i),
   simp_rw [mul_assoc, mul_left_comm _ (A' _ _), ←adjugate_eq_det_submatrix],
 end
+
+lemma det_eq_sum_mul_adjugate_col {n : Type*} [decidable_eq n] [fintype n]
+  (A : matrix n n R) (j : n) :
+  det A = ∑ i : n, A i j * adjugate A j i :=
+by simpa only [det_transpose, ←adjugate_transpose] using det_eq_sum_mul_adjugate_row Aᵀ j
