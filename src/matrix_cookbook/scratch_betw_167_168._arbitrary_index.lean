c2c897575eb7027c353205e2cb293ae3b22423ad
Mohanad Ahmed <m.a.m.elhassan@gmail.com>
2023-05-28 22:16:49 +0300

    Finally it works

--- src/matrix_cookbook/scratch_betw_167_168._arbitrary_index.lean
+++ src/matrix_cookbook/scratch_betw_167_168._arbitrary_index.lean
@@ -14,9 +14,9 @@ variable z : ℕ
 variable X : matrix (m ⊕ unit) m R
 variable v : matrix (m ⊕ unit) unit R
 def append_mat_vec := of (λ i, sum.elim (X i) (v i))
-def e := @fin_sum_fin_equiv z 1
+-- def e := @fin_sum_fin_equiv z 1
 
-lemma fin_one_mul_eq_smul' {A: matrix unit m R} {q: matrix unit unit R}:
+lemma fin_one_mul_eq_smul {A: matrix unit m R} {q: matrix unit unit R}:
   (q () ()) • A = q ⬝ A := 
 begin
   funext r s,
@@ -25,7 +25,7 @@ begin
   congr,
 end
 
-lemma rank_one_update_transpose_mul_self (z : ℕ)
+lemma rank_one_update_transpose_mul_self
   (X : matrix (m ⊕ unit) m R)
   (v : matrix (m ⊕ unit) unit R)
   [invertible (Xᵀ ⬝ X)] :
@@ -41,9 +41,9 @@ begin
     mul_apply, transpose_apply],},
 end
 
-lemma sS11'
-  {X : matrix (m ⊕ unit) m R}
-  {v : matrix (m ⊕ unit) unit R}
+lemma sS11
+  (X : matrix (m ⊕ unit) m R)
+  (v : matrix (m ⊕ unit) unit R)
   {hα: (vᵀ ⬝ v - vᵀ ⬝ X ⬝ (Xᵀ ⬝ X)⁻¹ ⬝ Xᵀ ⬝ v) 0 0 ≠ 0}
   [invertible (Xᵀ ⬝ X)] :
   let A : matrix m m R := (Xᵀ ⬝ X)⁻¹,
@@ -62,7 +62,7 @@ begin
     mul_nonsing_inv_cancel_left _ _ hA, ← matrix.mul_assoc],
 end
 
-lemma sS12' 
+lemma sS12 
   (X : matrix (m ⊕ unit) m R)
   (v : matrix (m ⊕ unit) unit R)
   [invertible (Xᵀ ⬝ X)] :
@@ -81,7 +81,7 @@ begin
   apply is_unit_det_of_invertible,
 end
 
-lemma sS21' (z : ℕ)
+lemma sS21
   (X : matrix (m ⊕ unit) m R)
   (v : matrix (m ⊕ unit) unit R)
   [invertible (Xᵀ ⬝ X)] :
@@ -97,14 +97,14 @@ begin
   change S21 with -vᵀ ⬝ X ⬝ Aᵀ,
   simp_rw [matrix.mul_add, matrix.mul_smul, matrix.neg_mul, matrix.mul_neg],
   change α  with  (vᵀ ⬝ v - vᵀ ⬝ X ⬝ A ⬝ Xᵀ ⬝ v) () (),
-  simp_rw [pi.sub_apply, sub_smul, fin_one_mul_eq_smul', ← sub_eq_add_neg,
+  simp_rw [pi.sub_apply, sub_smul, fin_one_mul_eq_smul, ← sub_eq_add_neg,
     transpose_nonsing_inv, transpose_mul, transpose_transpose, ← matrix.mul_assoc,
     add_sub_assoc, sub_add_sub_cancel, sub_self],
 end
 
-lemma sS22'
-  {X : matrix m m R}
-  {v : matrix m unit R}
+lemma sS22
+  (X : matrix (m ⊕ unit) m R)
+  (v : matrix (m ⊕ unit) unit R)
   {hα: (vᵀ ⬝ v - vᵀ ⬝ X ⬝ (Xᵀ ⬝ X)⁻¹ ⬝ Xᵀ ⬝ v) 0 0 ≠ 0}
   [invertible (Xᵀ ⬝ X)]:
   let A : matrix m m R := (Xᵀ ⬝ X)⁻¹,
@@ -124,38 +124,39 @@ begin
     neg_add_eq_sub, ← matrix.mul_assoc],
 end
 
--- lemma eq_between_167_and_168 [invertible (Xᵀ⬝X)] 
---   {hα: (vᵀ ⬝ v - vᵀ ⬝ X ⬝ (Xᵀ ⬝ X)⁻¹ ⬝ Xᵀ ⬝ v) 0 0 ≠ 0}: 
--- let 
---   X' := reindex (equiv.refl (m ⊕ unit)) (e z) (append_mat_vec X v), 
---   A := (Xᵀ⬝X)⁻¹,
---   α := ((vᵀ⬝v) - (vᵀ⬝X⬝A⬝Xᵀ⬝v)) 0 0,
---   S11 := α•A + (A⬝Xᵀ⬝v⬝vᵀ⬝X⬝Aᵀ),    S12 := -A⬝Xᵀ⬝v,
---   S21 := -vᵀ⬝X⬝Aᵀ,                  S22 := (1: matrix (fin 1) (fin 1) R),
---   S :=  (1/α)•from_blocks S11 S12 S21 S22
--- in 
---   (X'ᵀ⬝ X')⁻¹ = reindex (e z) (e z) S := begin
---   intros X' A α S11 S12 S21 S22 S,
---   change X' with reindex (equiv.refl (fin (z+1))) (e z) (append_mat_vec z X v),
+lemma eq_between_167_and_168 [invertible (Xᵀ⬝X)] 
+  {hα: (vᵀ ⬝ v - vᵀ ⬝ X ⬝ (Xᵀ ⬝ X)⁻¹ ⬝ Xᵀ ⬝ v) 0 0 ≠ 0}: 
+let 
+  X' := (append_mat_vec X v), 
+  A := (Xᵀ⬝X)⁻¹,
+  α := ((vᵀ⬝v) - (vᵀ⬝X⬝A⬝Xᵀ⬝v)) 0 0,
+  S11 := α•A + (A⬝Xᵀ⬝v⬝vᵀ⬝X⬝Aᵀ),    S12 := -A⬝Xᵀ⬝v,
+  S21 := -vᵀ⬝X⬝Aᵀ,                  S22 := (1: matrix unit unit R),
+  S :=  (1/α)•from_blocks S11 S12 S21 S22
+in 
+  (X'ᵀ⬝ X')⁻¹ = S := begin
+  intros X' A α S11 S12 S21 S22 S,
+  change X' with (append_mat_vec X v),
   
---   let Zre := reindex_linear_equiv_apply ℕ R (e z) (e z),
---   let Zr1 := reindex_linear_equiv_apply ℕ R (e z) (equiv.refl (fin (z+1))),
---   let Zr2 := reindex_linear_equiv_apply ℕ R (equiv.refl (fin (z+1))) (e z),
---   let Zr3 := reindex_linear_equiv_mul ℕ R (e z) (equiv.refl (fin (z+1))) (e z),
-  
---   rw [matrix.transpose_reindex, ← Zre, ← Zr1, ← Zr2, 
---     Zr3 (append_mat_vec z X v)ᵀ ((append_mat_vec z X v)), 
---     Zre, Zre], 
-
---   simp_rw [matrix.inv_reindex (e z) (e z), reindex_equiv_eq_iff_matrix_eq,
---     rank_one_update_transpose_mul_self],
---   apply inv_eq_right_inv,
---   rw [matrix.mul_smul, from_blocks_multiply, from_blocks_smul, ← from_blocks_one],
---   congr,
---   apply sS11, assumption',
---   apply sS12,
---   apply sS21,
---   apply sS22, assumption',
--- end
+  rw rank_one_update_transpose_mul_self,
+  apply inv_eq_right_inv,
+  rw [matrix.mul_smul, from_blocks_multiply, from_blocks_smul, ← from_blocks_one],
+  congr,
+  apply sS11,
+  exact hα,
+  apply sS12,
+  apply sS21,
+  apply sS22,
+  exact hα,
+  -- { have f1 : ∀ (r: unit), r = 0, {intro r, simp only [eq_iff_true_of_subsingleton],},
+  --   rw [one_div, inv_smul_eq_iff₀ hα, matrix.mul_one],
+  --   change S12 with -A ⬝ Xᵀ ⬝ v,
+  --   change α  with  (vᵀ ⬝ v - vᵀ ⬝ X ⬝ A ⬝ Xᵀ ⬝ v) 0 0,
+  --   simp_rw [matrix.neg_mul, matrix.mul_neg],
+  --   funext a b,
+  --   simp_rw [f1 a, f1 b, pi.smul_apply, one_apply_eq, algebra.id.smul_eq_mul, mul_one, 
+  --     neg_add_eq_sub, ← matrix.mul_assoc],
+  -- }
+end
 
 end matrix_cookbook
