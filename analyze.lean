88d306aaf9eab9e06b591efe2eb578a269d6afd5
Eric Wieser <wieser.eric@gmail.com>
2024-01-14 16:51:21 +0000

    analyze: add legend

--- analyze.lean
+++ analyze.lean
@@ -42,10 +42,10 @@ inductive Status
   | proved
   deriving DecidableEq, Hashable
 
-instance : Repr Status where
-  reprPrec r _ := match r with
+instance : ToString Status where
+  toString
   | .missing => "missing"
-  | .notStated => "notStated"
+  | .notStated => "not stated"
   | .stated => "stated"
   | .proved => "proved"
 
@@ -85,26 +85,40 @@ def get_url : IO (System.FilePath → Option DeclarationRange → String) := do
     | none => ""
 
 def make_svg (cells : List (ℕ × Option String × Status)) : Id String := do
+  let mut counts := Std.mkHashMap
+  for s in [Status.missing, Status.notStated, Status.stated, Status.proved] do
+    counts := counts.insert s 0
+  for (_, _, s) in cells do
+    counts := counts.modify s (fun _ v => v + 1)
+
   let svg := fun c =>
-    f!"<svg id=\"svg\" width=\"{550*2}\" height=\"25\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">"
+    f!"<svg id=\"svg\" width=\"{550*2}\" height=\"45\" version=\"1.1\" xmlns=\"http://www.w3.org/2000/svg\">"
+    ++ "<style>" ++ .nest 2 (
+      "text { color: gray; font-size: 16px; font-family: -apple-system,BlinkMacSystemFont,\"Segoe UI\",\"Noto Sans\",Helvetica,Arial,sans-serif,\"Apple Color Emoji\",\"Segoe UI Emoji\"}"
+    ) ++ "</style>"
     ++ c ++
     "</svg>"
-
+  let width := 2
   let rects ← cells.mapM (fun c : ℕ × Option String × Status => do
-    let color := match c.2.2 with
+    let r := f!"
+      <rect fill=\"{color c.2.2}\" x=\"{c.1*width}\" y=\"0\" width=\"{width}\" height=\"25\">
+        <title>{c.1}</title>
+      </rect>"
+    match c.2.1 with
+    | none => r
+    | some f => pure f!"<a href=\"{f}\">{r}</a>")
+
+  let legendEntries ← counts.toList.mapM fun (k, v) => do
+    f!"<tspan fill=\"{color k}\">{k}: {v} <tspan style=\"opacity: 0.75\">({v*100/cells.length}%)</tspan></tspan>"
+  let legend := f!"<text x=\"0\" y=\"45\" width=\"{width*cells.length}\">{Format.joinSep legendEntries ", "}</text>"
+
+  toString $ svg (Format.joinSep rects "\n" ++ legend)
+where
+  color : Status → String
     | .missing => "darkred"
     | .notStated => "red"
     | .stated => "yellow"
     | .proved => "green"
-    let r := f!"
-      <rect fill=\"{color}\" x=\"{c.1*2}\" y=\"0\" width=\"2\" height=\"25\">
-        <title>{c.1}</title>
-      </rect>"
-
-    match c.2.1 with
-    | none => r
-    | some f => pure f!"<a href=\"{f}\">{r}</a>")
-  toString $ svg (Format.joinSep rects "\n")
 
 def printSvg : CoreM String := do
   let get_url ← get_url
