2677149674403ed96843d790f9522ff263fe759d
Mohanad Ahmed <m.a.m.elhassan@gmail.com>
2024-06-30 02:58:30 +0300

    dft Matrices third attempt

--- MatrixCookbook/9SpecialMatrices.lean
+++ MatrixCookbook/9SpecialMatrices.lean
@@ -8,6 +8,7 @@ import Mathlib.LinearAlgebra.Matrix.PosDef
 import Mathlib.LinearAlgebra.Matrix.SchurComplement
 import Mathlib.LinearAlgebra.Matrix.Symmetric
 import Mathlib.LinearAlgebra.Vandermonde
+import Mathlib.Tactic
 
 /-! # Special Matrices -/
 
@@ -26,6 +27,8 @@ open Matrix
 open scoped BigOperators
 
 open scoped Matrix
+
+open Real Complex
 
 namespace MatrixCookbook
 
@@ -90,22 +93,111 @@ theorem eq_402 (A₁₁ : Matrix m m R) (A₂₂ : Matrix n n R) :
 
 
 /-! ## Discrete Fourier Transform Matrix, The -/
+noncomputable def Wₙ {N: ℕ}: Matrix (Fin N) (Fin N) ℂ :=
+  fun k n => Complex.exp (-2 * π * I * k * n / N)
 
+-- -- Conjugate DFT Matrix: Just the complex conjugate
+-- noncomputable def sWₙ {N: ℕ} : Matrix (Fin N) (Fin N) ℂ :=
+--   fun k n => Complex.exp (2 * π * I * k * n / N)
 
-theorem eq_403 : (sorry : Prop) :=
-  sorry
+noncomputable def iWₙ {N: ℕ} : Matrix (Fin N) (Fin N) ℂ :=
+  fun k n => (1/N) * Complex.exp (2 * π * I * k * n / N)
+lemma mod_eq_mod_neg (m a : ℤ) : Int.mod (-a) m = -Int.mod (a) m := by
+  rw [Int.mod_def, Int.mod_def, Int.neg_div, neg_sub', mul_neg, sub_neg_eq_add]
 
-theorem eq_404 : (sorry : Prop) :=
-  sorry
+lemma cexp_sub_ne_one {N : ℕ} (k p : Fin N) (h : (k ≠ p)) :
+    Complex.exp (2 * π * I * (k - p) / N) ≠ 1 := by
+  by_cases hN : N = 0
+  exfalso
+  apply Fin.elim0 (by convert k; exact hN.symm)
+  by_contra hg
+  rw [Complex.exp_eq_one_iff] at hg
+  obtain ⟨z, hz⟩ := hg
+  rw [mul_div_assoc, mul_comm (z:ℂ) _, (mul_right_inj' two_pi_I_ne_zero),
+      (div_eq_iff_mul_eq (Nat.cast_ne_zero.2 hN))] at hz
+  norm_cast at hz
+  apply_fun ( Int.mod · N) at hz
+  simp only [Int.mul_mod_left, Int.subNatNat_eq_coe] at hz
+  by_cases h1 : p ≤ k
+  · rw [Int.mod_eq_of_lt, eq_comm, sub_eq_zero] at hz
+    norm_cast at hz
+    apply h (Fin.ext hz)
+    simp only [sub_nonneg, Nat.cast_le, Fin.val_fin_le]
+    exact h1
+    rw [← Nat.cast_sub]
+    norm_cast
+    apply Nat.sub_lt_right_of_lt_add h1
+    apply Nat.lt_add_right _
+    apply Fin.is_lt
+    exact_mod_cast h1
+  · push_neg at h1
+    rw [ ← neg_sub, mod_eq_mod_neg, eq_comm, neg_eq_zero, Int.mod_eq_of_lt, sub_eq_zero,
+      eq_comm] at hz
+    norm_cast at hz
+    apply h (Fin.ext hz)
+    simp only [neg_sub, sub_nonneg, Nat.cast_le, Fin.val_fin_le]
+    apply le_of_lt h1
+    rw [← Nat.cast_sub]
+    norm_cast
+    apply Nat.sub_lt_right_of_lt_add (le_of_lt h1)
+    apply Nat.lt_add_right _
+    apply Fin.is_lt
+    apply le_of_lt
+    exact_mod_cast h1
 
-theorem eq_405 : (sorry : Prop) :=
-  sorry
+theorem inv_Wₙ {N: ℕ} : Wₙ⁻¹ = @iWₙ N := by
+  rw [inv_eq_left_inv]
+  funext p q
+  rw [mul_apply]
+  unfold Wₙ iWₙ
+  by_cases hN : N = 0
+  · exfalso
+    apply Fin.elim0 (by convert p; exact hN.symm)
+  -- N ≠ 0
+  simp_rw [mul_assoc (1/N:ℂ), ←Complex.exp_add, ←Finset.mul_sum, neg_mul, neg_div, ←sub_eq_add_neg,
+    ← sub_div, mul_assoc (2*π*I), ← mul_sub, mul_comm (p:ℂ) _, ← mul_sub]
+  have (a b c: Fin N) : cexp (2 * π * I * (a * (b - c)) / N) =  cexp (2 * π * I * (b - c) / N) ^ (a:ℕ) := by
+    rw [← Complex.exp_nat_mul]
+    ring_nf
+  simp_rw [this]
+  clear this
+  by_cases h : p = q
+  · simp_rw [h, sub_self, mul_zero, zero_div, Complex.exp_zero, one_pow, Fin.sum_const, nsmul_eq_mul,
+      mul_one, one_apply_eq]
+    apply div_mul_cancel₀
+    simp only [ne_eq, Nat.cast_eq_zero]
+    exact hN
+  -- p ≠ q
+  rw [one_apply_ne h, Fin.sum_univ_eq_sum_range, geom_sum_eq, ← Complex.exp_nat_mul,
+      mul_div_cancel₀, one_div_mul_eq_div]
+  rw [div_eq_zero_iff, div_eq_zero_iff]
+  left
+  left
+  rw [sub_eq_zero, Complex.exp_eq_one_iff]
+  use (p - q)
+  norm_cast
+  ring
+  simp only [ne_eq, Nat.cast_eq_zero]
+  exact hN
+  apply cexp_sub_ne_one _ _ h
 
-theorem eq_406 : (sorry : Prop) :=
-  sorry
+theorem eq_403 {N : ℕ} (k n : Fin N) :  Wₙ k n = Complex.exp (-2 * π * I * k * n / N) := rfl
 
-theorem eq_407 : (sorry : Prop) :=
-  sorry
+noncomputable def dft {N: ℕ} (x: (Fin N) → ℂ) : (Fin N → ℂ) :=
+  fun (k: Fin N) => ∑ (n : Fin N), (Wₙ k n) * (x n)
+
+noncomputable def idft {N: ℕ} (X: (Fin N) → ℂ) : (Fin N → ℂ) :=
+  fun (n: Fin N) => (1 / N) * ∑ (k : Fin N), (Wₙ (-k) n) * (X k)
+
+theorem eq_404 {N : ℕ} (x : Fin N → ℂ) (k : Fin N): (dft x) k =  ∑ (n : Fin N), (Wₙ k n) * (x n) := rfl
+
+theorem eq_405 {N : ℕ} (X : Fin N → ℂ) (n : Fin N): (idft X) n =  (1 / N) * ∑ (k : Fin N), (Wₙ (-k) n) * (X k) := by
+  simp only [idft, one_div]
+
+theorem eq_406 {N : ℕ} (x : Fin N → ℂ) : (dft x) = Matrix.mulVec Wₙ x := rfl
+
+-- theorem eq_407 {N : ℕ} (X : Fin N → ℂ) : (idft X) = Matrix.mulVec  :=
+--   sorry
 
 theorem eq_408 : (sorry : Prop) :=
   sorry
